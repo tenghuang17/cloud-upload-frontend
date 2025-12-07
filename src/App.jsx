dda1c6be26c6692da758740f02356e5b9af2a82a
TengHuang Hsueh <128714099+tenghuang17@users.noreply.github.com>
2025-12-07 22:01:56 +0800

    success upload to s3 (微調)

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ function App() {  // 定義的一個元件（component） 函式 = 元件
         contentType: file.type || "application/octet-stream"
       })
     });
+    // const formData = new FormData();
+    // formData.append("file", file);
+    // const res = await fetch(BACKEND_URL, {
+    //   method: "POST",
+    //   body: formData      //不要自己加 Content-Type，瀏覽器會自動加 boundary
+    // });
     if (!res.ok){
       throw new Error("Failed: " + res.status)
     }
@@ -35,7 +41,7 @@ function App() {  // 定義的一個元件（component） 函式 = 元件
     const signed = await getSignedUrl(file);     // signed json object  (like dictionary)
     const uploadUrl = signed.presigned_url;
     seturlResult(
-      `presigned url: ${uploadUrl}\nexpired in: 1000 seconds\n`
+      `presigned url: ${uploadUrl}\nexpired in: 120 seconds\n`
     ); // pre 只能顯示字串 不能顯示物件 
     const putRes = await fetch(uploadUrl, {
       method: "PUT",
@@ -44,12 +50,11 @@ function App() {  // 定義的一個元件（component） 函式 = 元件
       },
       body: file
     });
-
     if (!putRes.ok) {
       alert("Upload failed: " + putRes.status);
       return;
     }
-    
+
     setuploadResult(
       `Upload success!\nS3 Key: ${signed.key}\nPublic URL (if your bucket allows): https://your-bucket.s3.amazonaws.com/${signed.key}`
     ); // pre 只能顯示字串 不能顯示物件 
@@ -59,21 +64,18 @@ function App() {  // 定義的一個元件（component） 函式 = 元件
   return (            // using JSX describe UI
     <div style={{ padding: "50px" }}> 
       <h1>Upload to S3</h1>  
-
       <input          // 一個標籤 、React element
         type="file"   //  HTML:attribute  JSX:prop (property，屬性)
         onChange={(e) => setFile(e.target.files[0])}
         //   選檔 ->onChange 觸發 ->React捕捉change事件 ->React呼叫箭頭函式
         //   e:event object整個事件 被作為參數送進函式 -> 執行函式   沒有e=沒參數
       /> 
-      
       <button 
         style={{ display: "block", marginTop: "10px" }} 
         onClick={handleUpload}
       >
         Start! 
       </button>
-
       <pre>{urlResult}</pre> 
       <pre>{uploadResult}</pre>
     </div>
